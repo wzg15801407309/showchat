cc9c98ae54e210131ee2ef0dd5c36d5a8d17918c
zhuqin.wei <zhuqin.wei@zhuqinweideMacBook-Pro.local>
2021-01-13 11:35:58 +0800

    20210113信息显示

--- js/showchat.js
+++ js/showchat.js
@@ -5,6 +5,7 @@
         var init = function(opt){
             if(JSON.stringify(opt) == "{}") return;
             msgBoxStyle(opt);
+            msgShow(opt);
         };
         /**
          * 信息显示方式 
@@ -15,14 +16,53 @@
         var msgBoxStyle = function(opt){
             var styleID = opt.msgStyle || 0;
             if(styleID === 0 ){
-              return _isTopMsg = false;
+              return _isTopMsg = true;
             }
             var data=opt.styleData;
             var html = '<div class="system-message">'
-            +'正在和'+ data.client + '聊天'
+            +'正在和'+ data.clientNaem + '聊天'
             +'</div>'
             +'<div class="system-message">'+data.timestr+'</div>';
             $('.chat-window').append(html);
+        };
+        /**
+         * 显示信息
+         * @param {*} opt.msgData
+         * 类型：数组
+         * 
+         */
+        var msgShow = function(opt){
+            var data = opt.msgData || [];
+            if(data.length == 0)return;
+            $('.chat-window').append(msgSet(data));
+        };
+        /**
+         * 信息处理
+         * @param {*} data 
+         */
+        var msgSet = function(data){
+            var html='';
+            data.forEach(data => {
+                html += `<div class="left-box">
+                <div class="left-time ${_isTopMsg?'hiden' : ''}" >${data.Ctime}</div>
+                 <div class="left-wrapper">
+                     <div class="left-avatar">
+                         <img src="./image/group.png" alt="..." />
+                     </div>
+                     <div class="left-message" id=${data.id}>${data.msgClient}</div>
+                 </div>
+             </div>
+             <div class="right-box">
+                <div class="right-time ${_isTopMsg?'hiden' : ''}" >${data.CStime}</div>
+                 <div class="right-wrapper">
+                     <div class="right-message"  id=${data.id}>${data.msgCustomer}</div>
+                     <div class="right-avatar">
+                          <img src="./image/group.png" alt="..." />
+                     </div>
+                 </div>
+             </div>`
+            });
+            return html;
         }
         init(_options);
     };
