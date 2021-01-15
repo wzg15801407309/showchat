47ba8b19b0986b8a0298b6434eeebfdf806122c2
zhuqin.wei <zhuqin.wei@zhuqinweideMacBook-Pro.local>
2021-01-15 18:06:14 +0800

    20210115界面03

--- chat-03/js/pluginChat.js
+++ chat-03/js/pluginChat.js
@@ -3,6 +3,7 @@
  *  title：信息框的标题
  *  width: 信息框的宽度，默认100%
  *  height: 信息框的高度，默认100%
+ *  msgdata：显示的data 
  */
 ;(function(){
     function PluginChat(options){
@@ -11,17 +12,44 @@
     PluginChat.prototype = {
         constructor: PluginChat,
         pcContentId:'dpn-content',
+        msgDataTotal:[],
         pcInit: function (){
             console.log('PluginChat-init');
             // 创建基本dom
             this.createBaseDom();
-            
         },
         showMsg: function(data){
-            console.log("show",data);
+            if(data.length<=0)return
+            var _that = this;
+            var frag = document.getElementById(this.pcContentId);
+            var template = '';
+            for (var i = 0; i < data.length; i++) {
+                var addr = data[i];
+                template += `
+                <div class="left-wrapper">
+                    <div class="left-time">${addr.Ctime}</div>
+                    <div class="left-msgbox">
+                    <div class="left-avatar">
+                        <img src="./image/client.png" alt="">
+                    </div>
+                    <div class="left-message" id=${addr.id}>${addr.msgClient}</div>
+                    </div>
+                </div>
+                <div class="right-wrapper">
+                    <div class="right-time">${addr.CStime}</div>
+                    <div class="right-msgbox">
+                    <div class="right-message" id=${addr.id}>${addr.msgCustomer}</div>
+                    <div class="right-avatar">
+                        <img src="./image/customer.png" alt="">
+                    </div>
+                    </div>
+                </div>`          
+            }
+            frag.innerHTML=(template);
         },
         createBaseDom: function(){
-            var _options = this.options;
+            var _that = this;
+            var _options = _that.options;
             //获取父元素
             var frag = !_options.domID ? document.body : document.getElementById(_options.domID);
             var dpnMsgBox=document.createElement('div');
@@ -34,32 +62,13 @@
                             ${_options.title}
                         </div>
                     </div>
-                    <div id=${this.pcContentId} class="dpn-content">
-                        ${_options.msgData.map(addr => `
-                            <div class="left-wrapper">
-                                <div class="left-time">${addr.Ctime}</div>
-                                <div class="left-msgbox">
-                                <div class="left-avatar">
-                                    <img src="./image/client.png" alt="">
-                                </div>
-                                <div class="left-message" id=${addr.id}>${addr.msgClient}</div>
-                                </div>
-                            </div>
-                            <div class="right-wrapper">
-                                <div class="right-time">${addr.CStime}</div>
-                                <div class="right-msgbox">
-                                <div class="right-message" id=${addr.id}>${addr.msgCustomer}</div>
-                                <div class="right-avatar">
-                                    <img src="./image/customer.png" alt="">
-                                </div>
-                                </div>
-                            </div>
-                        `).join('')}
+                    <div id=${_that.pcContentId} class="dpn-content">
                     </div>`;
-            console.log("flag",frag);
             frag.appendChild(dpnMsgBox);
+            frag=null;
+            _that.dpnMsgBox=dpnMsgBox;
+            _that.showMsg(_options.msgData);
         }
-
     }
     function init(options){
         // 参数初始化：传入的参数 替换 默认的参数
